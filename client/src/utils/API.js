59572817d20592cdbc1405e9c3d24ecad4cd8f64
Lucas Condon <lcondon@bennington.edu>
2018-10-04 12:48:07 -0400

    https

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default {
   searchArticles: function(searchTerm) {
     return axios.get(
-      `http://hn.algolia.com/api/v1/search?tags=story&query=${searchTerm}`
+      `https://hn.algolia.com/api/v1/search?tags=story&query=${searchTerm}`
     );
   }
 };
